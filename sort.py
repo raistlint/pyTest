078dd6aafe4a8c99f2da41126e82c15afa3ab19e
rgao <rgao@MOB71200.hq.k.grp>
2018-04-03 18:30:50 +0800

    add quick sort - not complete

--- sort.py
+++ sort.py
@@ -1,13 +1,12 @@
 import copy
+import random
 
 def bubbleSort(list):
     for i in range(len(list)):
         for j in range(i+1, len(list)):
 #             print(i,j)
             if(list[i]>list[j]):
-                a = list[i]
-                list[i] = list[j]
-                list[j] = a
+                (list[i],list[j]) = (list[j], list[i])
     return list
         
 def test_bubbleSort():
@@ -19,6 +18,53 @@ def test_bubbleSort():
     print(bubbleSort(org))
     print(org)
     print("------------")
+
+# quick sort
+def partition(list, s, e):
+    pass
+def quickDo(list, start, end):
+    if start<end:
+        p = partition(list,start, end)
+        quickDo(list, start, p-1)
+        quickDo(list, p+1, end)
+def sortQuick(list):
+    quickDo(list, 0, len(list))
+    
+    
+def random_list(start, stop, length):
+    re = []
+    for i in range(length):
+        re.append(random.randint(start,stop))
+    return re
+        
+def test_sort(f):
+    a = []
+    if f(a) :
+        print("-f-: empty list")
+    else:
+        print("-p-: enmpty list")
+    for _ in range(10):
+        start = 0
+        end = 100
+        length = 20
+        a = random_list(start, end, length)
+        b = copy.copy(a)
+        org = copy.copy(a)
+        b.sort()
+        f(a)
+        for i in range(length):
+            if a[i] != b[i]:
+                print("-f-: in index {}, re={}, exp={}".format(i, a[i], b[i]))
+                print("\torg is", org)
+                print("\tre  is", a)
+                print("\texp is", b)
+                break
+#         print("\torg is", org)
+#         print("\tre  is", a)
+#         print("\texp is", b)
+        print("-pass-")
     
 if __name__ == "__main__":
-    test_bubbleSort()
+#     test_bubbleSort()
+    test_sort(bubbleSort)
+    print("last line")
