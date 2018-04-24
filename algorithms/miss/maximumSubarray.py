33897d8884ff07ef62e8dc731f3b2105db7d63a7
rgao <rgao@MOB71200.hq.k.grp>
2018-04-24 18:40:35 +0800

    add maximum Subarray draft

---
+++ algorithms/miss/maximumSubarray.py
@@ -0,0 +1,17 @@
+# the maximum subarray in chapter 4.1
+import unittest
+
+def findMaxCrossing(A,low,mid,high):
+    pass
+
+def findMaximum(A,low,high):
+    pass
+
+########################
+# Test section
+
+class TestFunSelection(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
